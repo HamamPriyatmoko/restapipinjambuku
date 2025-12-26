fa5e1bfe28228b2c55ad25b66688613d2f2a20d1
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2025-12-27 00:28:03 +0700

    fixing dockerfile dan add docker ignore

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,14 @@
-# FROM node
+FROM node:22-alpine
+
+WORKDIR app
+COPY package*.json .
+
+RUN npm install
+
+RUN npm install nodemon
+
+COPY . .
+
+RUN npx prisma generate
+
+EXPOSE 3000
--- src/routes/api.js
+++ src/routes/api.js
@@ -4,6 +4,9 @@ import { createBorrow } from '../controller/borrow.controller.js';
 import { createMember } from '../controller/member.controller.js';
 
 const router = express.Router();
+router.get('/', (req, res) => {
+  res.json({ data: 'Hello World!' });
+});
 
 router.post('/api/books', createBook);
 
